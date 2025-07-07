cf78fd6349e6d7a9b7440f7d0ec0c8ecd0292bae
Huyvq <139686652+QHuyCoder@users.noreply.github.com>
2025-07-07 22:26:39 +0700

    Update orbit_calc.py

--- src/orbit_calc.py
+++ src/orbit_calc.py
@@ -1,1 +1,19 @@
+from skyfield.api import load
+
+def get_planet_position(planet_name='mars', year=2025, month=7, day=7):
+    planets = load('de421.bsp')
+    ts = load.timescale()
+
+    t = ts.utc(year, month, day)
+    earth = planets['earth']
+    planet = planets[planet_name]
+
+    astrometric = earth.at(t).observe(planet)
+    ra, dec, distance = astrometric.radec()
+
+    return {
+        'right_ascension': ra.hours,
+        'declination': dec.degrees,
+        'distance_km': distance.km
+    }
 
